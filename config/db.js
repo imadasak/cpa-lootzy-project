2ddc47709bdd49c8c5005ffcb14469cc203d53be
imadasak <imadasak@gmail.com>
2025-07-27 07:34:30 +0100

    Add DB_PORT to config

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT // <-- هذا هو السطر الجديد والمهم
 };
 
 const pool = mysql.createPool(dbConfig);
